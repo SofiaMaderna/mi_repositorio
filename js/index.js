6fa3349c1136ba695374401815c5b694f5bbdd8c
Sofimaderna1 <Maderna75486@gmail.com>
2024-01-20 05:52:16 -0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function saludar(){
+    console.log("Bienvenido");
+}
+
+
+function identificarUsuario() {
+    console.warn*(error);
+}
+
+function calcularImporteConIVA(importe, iva){
+    
+}
+
+identificarUsuario();
